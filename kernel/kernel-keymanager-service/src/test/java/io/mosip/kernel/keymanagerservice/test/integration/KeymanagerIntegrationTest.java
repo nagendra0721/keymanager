ff3625e936bbb20443b1f90e1984d393d2511d1d
nagendra0721 <nagendra0718@gmail.com>
2025-04-08 18:44:33 +0530

    MOSIP-40926: update test case
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keymanagerservice/test/integration/KeymanagerIntegrationTest.java
+++ kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keymanagerservice/test/integration/KeymanagerIntegrationTest.java
@@ -366,7 +366,7 @@ public class KeymanagerIntegrationTest {
 		ResponseWrapper<SignatureResponseDto> responseWrapper = objectMapper.readValue(
 				result.getResponse().getContentAsString(), new TypeReference<ResponseWrapper<SignatureResponseDto>>() {
 				});
-		assertThat(responseWrapper.getErrors().get(0).getErrorCode(), is("KER-KMA-005"));
+		assertThat(responseWrapper.getErrors().get(0).getErrorCode(), is("KER-KMS-002"));
 	}
 
 }
