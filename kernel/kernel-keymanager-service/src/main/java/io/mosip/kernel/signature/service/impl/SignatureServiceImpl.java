6101a12809da5916525e8c072db3786c2b580d6f
nagendra0721 <nagendra0718@gmail.com>
2026-01-13 18:57:58 +0530

    MOSIP-44250: allow expired certificate to verify jwt sign (#513)
    * MOSIP-44250: allow expired certifivate to verify jwt sign
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>
    
    * MOSIP-44250: Disable error logging for certificate date validation
    
    Comment out error logging for invalid certificate dates.
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>
    
    ---------
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/signature/service/impl/SignatureServiceImpl.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/signature/service/impl/SignatureServiceImpl.java
@@ -619,10 +619,10 @@ public class SignatureServiceImpl implements SignatureService, SignatureServicev
 			X509Certificate x509CertToVerify = (X509Certificate) certToVerify;
 			boolean validCert = SignatureUtil.isCertificateDatesValid(x509CertToVerify);
 			if (!validCert) {
-				LOGGER.error(SignatureConstant.SESSIONID, SignatureConstant.JWT_SIGN, SignatureConstant.BLANK,
-					"Error certificate dates are not valid.");
-					throw new CertificateNotValidException(SignatureErrorCode.CERT_NOT_VALID.getErrorCode(),
-								SignatureErrorCode.CERT_NOT_VALID.getErrorMessage());
+				// LOGGER.error(SignatureConstant.SESSIONID, SignatureConstant.JWT_SIGN, SignatureConstant.BLANK,
+				// 	"Error certificate dates are not valid.");
+//					throw new CertificateNotValidException(SignatureErrorCode.CERT_NOT_VALID.getErrorCode(),
+//								SignatureErrorCode.CERT_NOT_VALID.getErrorMessage());
 			}
 
 			String keyAlgorithm = x509CertToVerify.getPublicKey().getAlgorithm();
